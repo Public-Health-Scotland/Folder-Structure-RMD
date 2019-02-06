3da894fde65da4da463feb464651e6607b6b13e8
Ewout Jaspers <ejaspers@nhs.net>
2019-02-06 13:38:15 +0000

    new structure

--- RunScript.R
+++ RunScript.R
@@ -1,12 +1,28 @@
 # RMarkdown Script
 source("config.R")
 
-data_filepath <- file.path("basefilepath", "data")
-temp_filepath <- file.path("basefilepath", "temp")
-templates_filepath <- file.path("basefilepath", "templates")
-output_filepath <- file.path("basefilepath", "output")
-output_figures_filepath <- file.path("basefilepath", "output/figures")
-markdown_filepath <- file.path("basefilepath", "rmarkdown")
-markdown_filepath <- file.path("basefilepath", "script")
+data_filepath <- file.path(basefilepath, "data")
+temp_filepath <- file.path(basefilepath, "temp")
+output_filepath <- file.path(basefilepath, "output")
+output_figures_filepath <- file.path(basefilepath, "output/figures")
+rmarkdown_filepath <- file.path(basefilepath, "rmarkdown")
+scripts_filepath <- file.path(basefilepath, "code")
 
+## Create Summary
+
+summary_markdown <- paste0(rmarkdown_filepath,"/ISD-NATIONAL-STATS-SUMMARY.Rmd")
+
+summary_output <- 
+  paste0(output_filepath, "/summary.docx")
+
+rmarkdown::render(summary_markdown, output_file = summary_output)
+
+## Create Report
+
+report_markdown <- paste0(rmarkdown_filepath,"/ISD-NATIONAL-STATS-REPORT.Rmd")
+
+report_output <- 
+  paste0(output_filepath, "/report.docx")
+
+rmarkdown::render(report_markdown, output_file = report_output)
 
