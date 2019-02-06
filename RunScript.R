6415d51aea000c4c8339b970d322191393e5711d
Ewout Jaspers <ejaspers@nhs.net>
2019-02-06 12:08:08 +0000

    start of new structure

---
+++ RunScript.R
@@ -0,0 +1,12 @@
+# RMarkdown Script
+source("config.R")
+
+data_filepath <- file.path("basefilepath", "data")
+temp_filepath <- file.path("basefilepath", "temp")
+templates_filepath <- file.path("basefilepath", "templates")
+output_filepath <- file.path("basefilepath", "output")
+output_figures_filepath <- file.path("basefilepath", "output/figures")
+markdown_filepath <- file.path("basefilepath", "rmarkdown")
+markdown_filepath <- file.path("basefilepath", "script")
+
+
